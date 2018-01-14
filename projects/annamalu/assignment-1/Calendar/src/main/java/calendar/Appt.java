91b145678fc9fa58bd4b9824391d222c7c6dac26
Umayal Annamalai <annamalu@flip3.engr.oregonstate.edu>
2018-01-14 00:42:32 -0800

    Added bug #1 in isValid method

--- projects/annamalu/assignment-1/Calendar/src/main/java/calendar/Appt.java
+++ projects/annamalu/assignment-1/Calendar/src/main/java/calendar/Appt.java
@@ -110,7 +110,7 @@ public class Appt implements  Comparable<Appt>{
      * @sets valid to true if the appointment is valid
      */
     private void isValid() {
-    	int NumDaysInMonth= CalendarUtil.NumDaysInMonth(startYear,startMonth-1);
+    	int NumDaysInMonth= CalendarUtil.NumDaysInMonth(startYear,startMonth);
     				
     	if(startHour<0 || startHour>23)
     		this.valid=false;
